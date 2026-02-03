c4e1b25c5dd20b70308420360af96043fcaa3e0c
mjh-shikder <mjhsikder@gmail.com>
2026-02-03 19:58:15 +0600

    code for credentials log in added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,30 @@ import GithubProvider from "next-auth/providers/github"
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
-        
         // ...add more providers here
+        CredentialsProvider({
+            // Sign in with {name} button
+            name: "Credentials",
+            // Form Inputs
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
+        })
     ],
 }
 
