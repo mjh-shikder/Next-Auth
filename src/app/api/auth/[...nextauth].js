d805cae71cfb9089f64b82104473b64d88c3f8dd
mjh-shikder <mjhsikder@gmail.com>
2026-02-06 19:00:23 +0600

    env added

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
     // Configure one or more authentication providers
